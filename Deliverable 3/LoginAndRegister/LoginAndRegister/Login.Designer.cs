db7d00720e9fd54d327035ea844736b5c979c767
Dylan_Exton <dylexton@gmail.com>
2017-05-05 14:53:21 +1200

    Copied Deliverable 2 into Deliverable 3
    The starting point for Deliverable 3

---
+++ Deliverable 3/LoginAndRegister/LoginAndRegister/Login.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace LoginAndRegister
+{
+  partial class Login
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.butLogin = new System.Windows.Forms.Button();
+            this.butClear = new System.Windows.Forms.Button();
+            this.lnkRegister = new System.Windows.Forms.LinkLabel();
+            this.textBoxUsername = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.MaskedTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Impact", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(34, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(253, 34);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Login to the Database";
+            // 
+            // butLogin
+            // 
+            this.butLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.butLogin.Location = new System.Drawing.Point(110, 139);
+            this.butLogin.Name = "butLogin";
+            this.butLogin.Size = new System.Drawing.Size(82, 31);
+            this.butLogin.TabIndex = 3;
+            this.butLogin.Text = "Login";
+            this.butLogin.UseVisualStyleBackColor = true;
+            this.butLogin.Click += new System.EventHandler(this.butLogin_Click);
+            // 
+            // butClear
+            // 
+            this.butClear.Location = new System.Drawing.Point(110, 176);
+            this.butClear.Name = "butClear";
+            this.butClear.Size = new System.Drawing.Size(82, 32);
+            this.butClear.TabIndex = 5;
+            this.butClear.Text = "Clear";
+            this.butClear.UseVisualStyleBackColor = true;
+            this.butClear.Click += new System.EventHandler(this.butClear_Click);
+            // 
+            // lnkRegister
+            // 
+            this.lnkRegister.ActiveLinkColor = System.Drawing.Color.White;
+            this.lnkRegister.AutoSize = true;
+            this.lnkRegister.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lnkRegister.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.lnkRegister.Location = new System.Drawing.Point(88, 211);
+            this.lnkRegister.Name = "lnkRegister";
+            this.lnkRegister.Size = new System.Drawing.Size(126, 20);
+            this.lnkRegister.TabIndex = 6;
+            this.lnkRegister.TabStop = true;
+            this.lnkRegister.Text = "Register Now!!";
+            this.lnkRegister.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lnkRegister_LinkClicked);
+            // 
+            // textBoxUsername
+            // 
+            this.textBoxUsername.Location = new System.Drawing.Point(68, 74);
+            this.textBoxUsername.Name = "textBoxUsername";
+            this.textBoxUsername.Size = new System.Drawing.Size(146, 20);
+            this.textBoxUsername.TabIndex = 7;
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(68, 105);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.PasswordChar = '*';
+            this.textBoxPassword.Size = new System.Drawing.Size(146, 20);
+            this.textBoxPassword.TabIndex = 8;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(302, 246);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.textBoxUsername);
+            this.Controls.Add(this.lnkRegister);
+            this.Controls.Add(this.butClear);
+            this.Controls.Add(this.butLogin);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Login";
+            this.Text = "Login to the Database";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.Label label3;
+    private System.Windows.Forms.Button butLogin;
+    private System.Windows.Forms.Button butClear;
+    private System.Windows.Forms.LinkLabel lnkRegister;
+    private System.Windows.Forms.TextBox textBoxUsername;
+    private System.Windows.Forms.MaskedTextBox textBoxPassword;
+    }
+}
+
---
+++ Deliverable 3/Timeslot Generator/Timeslot Generator/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Timeslot_Generator
+{
+    public partial class Form1 : Form
+    {
+        //CONSTANTS
+        int minHour = 7, maxHour = 19, minDay = 1, maxDay = 31;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //ID can be between 1 to 4 on weekdays(1-5), and 1 on Saturdays(6)
+            //Time is between 7:00:00 and 20:00:00
+            //Date is between 0 and 31 days, skipping every 7th day
+
+            int day = 1;
+            int hour = 7;
+            int id = 1;
+
+            for(day = minDay; day <= maxDay; day++)
+            {
+                if(day % 6 == 0)
+                {
+                    id = 1;
+                   
+                    for(hour = minHour; hour <= maxHour; hour++)
+                    {
+                        SQL.executeQuery("INSERT INTO timeslots VALUES('" + id + "','" + hour.ToString() + ":00:00','2017-01-" + day.ToString() +" ')");
+                    }
+                }
+                else if(day % 7 == 0)
+                {
+                    
+                }
+                else
+                {
+                    id = 1;
+                  
+                    for(hour = minHour; hour <= maxHour; hour++)
+                    {
+                        for(id = 1; id <= 4; id++)
+                        {
+                            SQL.executeQuery("INSERT INTO timeslots VALUES('" + id + "','" + hour.ToString() + ":00:00','2017-01-" + day.ToString() + " ')");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
