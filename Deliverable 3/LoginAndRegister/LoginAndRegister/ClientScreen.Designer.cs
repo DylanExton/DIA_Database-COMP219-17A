b4a5d2d10f52bc53ccdf6674046b247ea901460a
Dylan_Exton <dylexton@gmail.com>
2017-05-18 13:03:59 +1200

    Client Screen Now Works
    This now rounds off the D3 as the client can use the application to book
    appointments given by a instructor.

--- Deliverable 3/LoginAndRegister/LoginAndRegister/ClientScreen.Designer.cs
+++ Deliverable 3/LoginAndRegister/LoginAndRegister/ClientScreen.Designer.cs
@@ -31,6 +31,16 @@
             this.label1 = new System.Windows.Forms.Label();
             this.labelID = new System.Windows.Forms.Label();
             this.buttonLogout = new System.Windows.Forms.Button();
+            this.comboBoxDate = new System.Windows.Forms.ComboBox();
+            this.comboBoxTime = new System.Windows.Forms.ComboBox();
+            this.comboBoxInstructor = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.listBoxAppointments = new System.Windows.Forms.ListBox();
+            this.buttonBook = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -61,11 +71,112 @@
             this.buttonLogout.UseVisualStyleBackColor = true;
             this.buttonLogout.Click += new System.EventHandler(this.buttonLogout_Click);
             // 
+            // comboBoxDate
+            // 
+            this.comboBoxDate.FormattingEnabled = true;
+            this.comboBoxDate.Location = new System.Drawing.Point(268, 111);
+            this.comboBoxDate.Name = "comboBoxDate";
+            this.comboBoxDate.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxDate.TabIndex = 3;
+            this.comboBoxDate.SelectedIndexChanged += new System.EventHandler(this.comboBoxDate_SelectedIndexChanged);
+            // 
+            // comboBoxTime
+            // 
+            this.comboBoxTime.FormattingEnabled = true;
+            this.comboBoxTime.Location = new System.Drawing.Point(446, 111);
+            this.comboBoxTime.Name = "comboBoxTime";
+            this.comboBoxTime.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxTime.TabIndex = 4;
+            // 
+            // comboBoxInstructor
+            // 
+            this.comboBoxInstructor.FormattingEnabled = true;
+            this.comboBoxInstructor.Location = new System.Drawing.Point(81, 111);
+            this.comboBoxInstructor.Name = "comboBoxInstructor";
+            this.comboBoxInstructor.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxInstructor.TabIndex = 6;
+            this.comboBoxInstructor.SelectedIndexChanged += new System.EventHandler(this.comboBoxInstructor_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(229, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Date:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(407, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Time:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(21, 114);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Instructor:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Impact", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(197, 67);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(216, 26);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Choose an Appointment";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Impact", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(216, 181);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(173, 26);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Your Appointments";
+            // 
+            // listBoxAppointments
+            // 
+            this.listBoxAppointments.FormattingEnabled = true;
+            this.listBoxAppointments.Location = new System.Drawing.Point(24, 210);
+            this.listBoxAppointments.Name = "listBoxAppointments";
+            this.listBoxAppointments.Size = new System.Drawing.Size(543, 173);
+            this.listBoxAppointments.TabIndex = 12;
+            // 
+            // buttonBook
+            // 
+            this.buttonBook.Location = new System.Drawing.Point(24, 138);
+            this.buttonBook.Name = "buttonBook";
+            this.buttonBook.Size = new System.Drawing.Size(543, 36);
+            this.buttonBook.TabIndex = 13;
+            this.buttonBook.Text = "Book Appointment";
+            this.buttonBook.UseVisualStyleBackColor = true;
+            this.buttonBook.Click += new System.EventHandler(this.buttonBook_Click);
+            // 
             // ClientScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(604, 395);
+            this.Controls.Add(this.buttonBook);
+            this.Controls.Add(this.listBoxAppointments);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBoxInstructor);
+            this.Controls.Add(this.comboBoxTime);
+            this.Controls.Add(this.comboBoxDate);
             this.Controls.Add(this.buttonLogout);
             this.Controls.Add(this.labelID);
             this.Controls.Add(this.label1);
@@ -82,5 +193,15 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label labelID;
         private System.Windows.Forms.Button buttonLogout;
+        private System.Windows.Forms.ComboBox comboBoxDate;
+        private System.Windows.Forms.ComboBox comboBoxTime;
+        private System.Windows.Forms.ComboBox comboBoxInstructor;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ListBox listBoxAppointments;
+        private System.Windows.Forms.Button buttonBook;
     }
 }
--- Deliverable 3/LoginAndRegister/LoginAndRegister/ClientScreen.cs
+++ Deliverable 3/LoginAndRegister/LoginAndRegister/ClientScreen.cs
@@ -13,6 +13,14 @@ namespace LoginAndRegister
     public partial class ClientScreen : Form
     {
         private string uName, fName, sName;
+
+        private BindingList<string> appointments = new BindingList<string>();
+        private BindingList<string> instructorIDs = new BindingList<string>();
+        private BindingList<string> instructorNames = new BindingList<string>();
+        private BindingList<string> dates = new BindingList<string>();
+        private BindingList<string> times = new BindingList<string>();
+        private BindingList<string> appID = new BindingList<string>();
+
         public ClientScreen()
         {
             InitializeComponent();
@@ -30,15 +38,114 @@ namespace LoginAndRegister
             }
         }
 
+        private void comboBoxInstructor_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            UpdateDates();
+            UpdateTimes();
+        }
+
+        private void comboBoxDate_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            UpdateTimes();
+
+        }
+
+        private void buttonBook_Click(object sender, EventArgs e)
+        {
+            string clientID = uName, appointmentID = appID[comboBoxTime.SelectedIndex];
+            try
+            {
+                SQL.executeQuery("UPDATE appointments SET client_id = '" + clientID+ "' where appointment_id = " + appointmentID);
+            }
+            catch
+            {
+                MessageBox.Show("Error with the inputs, please check they are not blank.", "Input error", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            }
+
+            UpdateAppointments();
+            UpdateDates();
+            UpdateTimes();
+        }
+
         private void ClientScreen_Load(object sender, EventArgs e)
         {
-            labelID.Text = "Logged in as " + fName + " " + sName + " (" + uName + ")"; ;
+            labelID.Text = "Logged in as " + fName + " " + sName + " (" + uName + ")";
+
+            UpdateAppointments();
+
+            SQL.selectQuery("SELECT * from instructors");
+            if (SQL.read.HasRows)
+            {
+                while (SQL.read.Read())
+                {
+                    instructorIDs.Add(SQL.read[0].ToString());
+                    instructorNames.Add(SQL.read[2].ToString() + " " + SQL.read[3].ToString());
+                }
+            }
+            comboBoxInstructor.DataSource = instructorNames;
         }
         public void getInfo(string uname, string fname, string sname)
         {
             uName = uname;
             fName = fname;
             sName = sname;
+        }
+
+        private void UpdateDates()
+        {
+            dates.Clear();
+            try
+            {
+                SQL.selectQuery("SELECT distinct slotDate from appointments where instructor_id like '" + instructorIDs[comboBoxInstructor.SelectedIndex] + "'");
+                if (SQL.read.HasRows)
+                {
+                    while (SQL.read.Read())
+                    {
+                        dates.Add(SQL.read[0].ToString());
+                    }
+                }
+                comboBoxDate.DataSource = dates;
+                comboBoxDate.SelectedIndex = 0;
+            }
+            catch
+            {
+            }
+        }
+        private void UpdateTimes()
+        {
+            times.Clear();
+            appID.Clear();
+            try
+            {
+                SQL.selectQuery("SELECT slotTime, appointment_id from appointments where instructor_id like '" + instructorIDs[comboBoxInstructor.SelectedIndex] +
+                    "' and slotDate like '" + dates[comboBoxDate.SelectedIndex] + "' and client_id is null and confirmed = 1");
+                if (SQL.read.HasRows)
+                {
+                    while (SQL.read.Read())
+                    {
+                        times.Add(SQL.read[0].ToString());
+                        appID.Add(SQL.read[1].ToString());
+                    }
+                }
+
+                comboBoxTime.DataSource = times;
+            }
+            catch
+            {
+            }
+        }
+        private void UpdateAppointments()
+        {
+            appointments.Clear();
+            SQL.selectQuery("SELECT * from appointments where client_id like'" + uName + "'");
+            if (SQL.read.HasRows)
+            {
+                while (SQL.read.Read())
+                {
+                    appointments.Add(SQL.read[0].ToString() + " " + SQL.read[3].ToString() + " " + SQL.read[4].ToString() + " " + SQL.read[7].ToString());
+                }
+            }
+            listBoxAppointments.DataSource = appointments;
         }
     }
 }
